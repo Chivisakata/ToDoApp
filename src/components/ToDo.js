6b4a39f47dcb153f78a915c798b54e4dd191500e
Vĩ Cao <vifreefire@gmail.com>
2026-01-07 22:29:02 +0700

    Add checkbutton for todo button

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,34 @@
 import React from "react"
 import Button from '@atlaskit/button'
+import CheckIcon from '@atlaskit/icon/glyph/check';
+import styled from "styled-components";
+
+const StyledButton = styled(Button)`
+  margin-top: 2px;
+  text-align: left;
+
+  &:hover {
+    .check-icon {
+      display: inline-block;
+    }
+}
+
+  .check-icon {
+  display: none;
+
+  &:hover {
+    background-color: #d5d1d1ff;
+    border-radius: 3px;
+  }
+}
+`;
 
 
-export default function ToDo({todo}) {
+export default function ToDo({todo, onCheckBtnClick}) {
   return (
     <>
-      <Button shouldFitContainer style={{marginTop:'2px', textAlign:'left'}}>{todo.name}</Button>
+      <StyledButton iconAfter={<span className="check-icon"><CheckIcon primaryColor="#4fff4f" onClick={() => onCheckBtnClick(todo.id)}/></span>} shouldFitContainer>{todo.name}</StyledButton>
     </>
   )
 }
+ 
